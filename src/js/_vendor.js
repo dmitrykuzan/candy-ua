3677bd7229c53e6ba8947c9d67711e9cc3e2b257
dmitrykuzan <dmitrykuzan@gmail.com>
2025-02-26 17:06:47 +0200

    fslightbox optimize

--- src/js/_vendor.js
+++ src/js/_vendor.js
@@ -1,8 +1,22 @@
 import "focus-visible";
 
-import "fslightbox";
-
 import "../../node_modules/swiper/swiper-bundle";
 
 import Swiper, { Navigation, Pagination } from "swiper";
 Swiper.use([Navigation, Pagination]);
+
+document.addEventListener("DOMContentLoaded", () => {
+  let fsLightboxLoaded = false;
+
+  document.querySelectorAll(".gallery__link").forEach((link) => {
+    link.addEventListener("click", async (event) => {
+      if (!fsLightboxLoaded) {
+        event.preventDefault();
+        await import("fslightbox");
+        fsLightboxLoaded = true;
+        link.click();
+        console.log("trueee");
+      }
+    });
+  });
+});
